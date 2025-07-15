ce8388b81b0eb1f1d5473cba9337932b5e7e79f7
Asad Ali <message.asadali@gmail.com>
2025-07-15 10:47:12 +0500

    logo added

--- src/components/FaceDetect.jsx
+++ src/components/FaceDetect.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FaceDetect = () => {
   return (
-    <div className='bg-red-500 w-[400px]'>FaceDetect</div>
+    <div className='bg-red-500 w-[400px] h-screen max-h-[400px]'>FaceDetect</div>
   )
 }
 
